bfaa97da7fa470db8f2e052d0b143b4ee8e1eb5b
Michael Hayes <tasslehawf@mac.com>
2013-10-02 14:19:28 -0500

    build-essential default config

--- attributes/default.rb
+++ attributes/default.rb
@@ -25,6 +25,8 @@ default["rackbox"]["default_config"]["passenger_runit"]["min_instances"] = 1
 default["rackbox"]["default_config"]["passenger_runit"]["spawn_method"] = "smart-lv2"
 default["rackbox"]["default_config"]["passenger_runit"]["host"] = "localhost"
 
+default["rackbox"]["default_config"]["build_essential"]["compiletime"] = true
+
 default["rackbox"]["apps"]["unicorn"] = []
 default["rackbox"]["apps"]["passenger"] = []
 
